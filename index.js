821f3e710c30895044836d59fb8d504db882e577
manishshukla00 <manishshuklacreative@outlook.com>
2024-05-30 11:08:20 +0530

    initial setup for API

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+import cors from "cors";
+
+const app = express();
+configDotenv();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((error) => console.log(error.message));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server is listening at port:${port}`));
